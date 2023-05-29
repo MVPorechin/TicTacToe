5e3f43cd37986ad594a9bd80415d0f60c48c9f9c
Maksim Porechin <111644533+MVPorechin@users.noreply.github.com>
2023-05-29 22:33:29 +0300

    Update Game.py

--- Game.py
+++ Game.py
@@ -35,32 +35,40 @@ class Player:
 
 
 class Game:
-    player_win = False
     wall_of_fame = dict()
+    players_list = list()
 
     # класс «Игры» содержит атрибуты:
     # состояние игры,
     # игроки,
     # поле.
-    def __init__(self, condition_game, players_list, field):
+    def __init__(self, condition_game, player_one, player_two, field):
         self.condition_game = condition_game
-        self.players_list = players_list
+        self.player_one = player_one
+        self.player_two = player_two
         self.field = field
 
     # Метод запуска одного хода игры.
     # Получает одного из игроков, запрашивает у игрока номер клетки, изменяет поле, проверяет, выиграл ли игрок.
     # Если игрок победил, возвращает True, иначе False.
 
-    def one_step(self, player):
-        player_ord_y, player_ord_x = [int(value) for value in input(f'{player.name} делает ход: ').split()]
-        self.field.change_value_cell(player.symbol, player_ord_y, player_ord_x)
-        self.field.print_board()
-        for line in self.rows() + self.columns() + self.diagonals():
-            sym = set(line)
-            if player.symbol in sym and len(sym) == 1:
-                return True
+    def add_player(self):
+        if len(self.players_list) == 0:
+            self.players_list.append(self.player_one)
+            self.players_list.append(self.player_two)
         else:
-            False
+            return self.players_list
+
+    def add_winner_to_rating(self, player):
+        if len(self.wall_of_fame) == 0:
+            for player in self.players_list:
+                self.wall_of_fame[player.name] = 0
+        else:
+            self.wall_of_fame[player.name] += 1
+
+    def print_wall_of_fame(self):
+        return [print(f'Игрок {key}, счет: {self.wall_of_fame[key]}') for key in
+                sorted(self.wall_of_fame, key=self.wall_of_fame.get, reverse=True)]
 
     def rows(self):
         return self.field.board[:]
@@ -73,39 +81,44 @@ class Game:
         slash = [self.field.board[index][len(self.field.board[index]) - index - 1] for index in range(3)]
         return [backslash, slash]
 
-    def cleanboard(self):
+    def one_step(self, player):
+        player_ord_y, player_ord_x = [int(value) for value in input(f'{player.name} делает ход: ').split()]
+        self.field.change_value_cell(player.symbol, player_ord_y, player_ord_x)
+        self.field.print_board()
+        for line in self.rows() + self.columns() + self.diagonals():
+            sym = set(line)
+            if player.symbol in sym and len(sym) == 1:
+                return True
+            # else:
+            #     return False
+
+    def clean_board(self):
         return [[self.field.change_value_cell('*', ord_y, ord_x) for ord_y in range(3)] for ord_x in range(3)]
 
     def currient_session(self):
+        self.add_player()
+        self.clean_board()
         self.field.print_board()
-        while not self.condition_game:
+        while True:
             for player in self.players_list:
                 if self.one_step(player):
                     print(f'{player.name} победил!')
-                    Game.wall_of_fame[player.name] = 1
-                    self.cleanboard()
+                    print('Доска победы')
+                    self.add_winner_to_rating(player)
                     self.print_wall_of_fame()
                     return True
-                    # Метод запуска одной игры.
+            # return False
+        # Метод запуска одной игры.
         # Очищает поле, запускает цикл с игрой, который завершается победой одного из игроков или ничьей.
         # Если игра завершена, метод возвращает True, иначе False.
-        pass
-
-    def print_wall_of_fame(self):
-        return [print(f'Игрок {key}, счет: {self.wall_of_fame[key]}') for key in
-                sorted(self.wall_of_fame, key=self.wall_of_fame.get, reverse=True)]
 
     def run_game(self):
-        self.currient_session()
-        while True:
-            ask_a_question = input('Игра завершена, запустить заново? "Да" или "Нет": ').lower()
-            if ask_a_question == 'да':
-                self.currient_session()
-                print('Доска победы')
-                # self.print_wall_of_fame()
-                return True
-            else:
-                break
+        while self.condition_game:
+            if self.currient_session():
+                ask_a_question = input('Игра завершена, запустить заново? "Да" или "Нет": ').lower()
+                if ask_a_question == 'нет':
+                    self.condition_game = False
+
 
     # Основной метод запуска игр.
     # В цикле запускает игры, запрашивая после каждой игры, хотят ли игроки продолжать играть.
